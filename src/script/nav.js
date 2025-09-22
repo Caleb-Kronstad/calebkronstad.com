0a863425563ec91371f3a983604546befc70edbd
Caleb-Kronstad <calebtkronstad@gmail.com>
2025-09-22 10:48:14 -0700

    projects page

---
+++ src/script/nav.js
@@ -0,0 +1,12 @@
+document.getElementById("home-btn").onclick = function() {
+    location.href = "..\\index.html";
+};
+document.getElementById("projects-btn").onclick = function() {
+    location.href = "..\\projects\\index.html";
+};
+document.getElementById("hobbies-btn").onclick = function() {
+    location.href = "..\\hobbies\\index.html"
+};
+document.getElementById("contact-btn").onclick = function() {
+    location.href = "..\\contact\\index.html"
+};
